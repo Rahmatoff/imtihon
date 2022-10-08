5917eb62a517b275084c616c31e8c106d66451e7
Rahmatovdev1 <github6767@gmail.com>
2022-10-08 20:56:20 +0500

    imtihon masalalari

---
+++ js/main.js
@@ -0,0 +1,88 @@
+//data types:
+
+// boolean, string, symbol, number, BigInt, undefined, null, object
+
+
+
+
+
+//----- 3 masala ---------
+
+// function uppercase(name){
+    
+    //         let nameArray = name.split(" ");
+    //         return nameArray[0][0].toUpperCase() + "." + nameArray[1][0].toUpperCase();
+    //       }
+    
+    
+    //     console.log(uppercase('rahmatov azizbek'));
+    
+
+
+
+
+
+
+
+    
+    
+    
+    // ------ 2 masala ------
+    
+    
+    
+    let newArr = [];
+    
+    function remove(keep = ['ali','vali','ali','vali', 3 , 7]){
+        for(let i = 0; i <keep.length; i++){
+            if(i % 2 === 0){
+               newArr.push(keep[i])
+            }
+        }
+        return newArr;
+    }
+    
+    
+    console.log(remove());
+
+
+
+
+    //  ------ 1 masala ------
+
+
+
+    function lastproblem(a,b){
+    
+    }
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
